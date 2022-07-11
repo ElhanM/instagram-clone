0eb983d2706c3ae48254ff03c6a04274b2c59a59
ElhanM <elhan.mahmutovic03@gmail.com>
2022-07-11 10:44:51 +0200

    formatting

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,20 +5,14 @@ const User = require("../models/User");
 exports.protect = async (req, res, next) => {
   let token;
 
-  if (
-    // req.headers.authorization &&
-    req.headers.authorization?.startsWith("Bearer")
-  ) {
+  if (req.headers.authorization?.startsWith("Bearer")) {
     token = req.headers.authorization.split(" ")[1];
   }
-
   if (!token) {
     return next(new ErrorResponse("Not authorized to access this route", 401));
   }
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     const user = await User.findById(decoded.id);
 
     if (!user) {
@@ -26,7 +20,6 @@ exports.protect = async (req, res, next) => {
     }
 
     req.user = user;
-
     next();
   } catch (err) {
     return next(new ErrorResponse("Not authorized to access this router", 401));
