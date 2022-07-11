4a9330c71d5d8db8e9d8ddd5cbccd0198125ed54
ElhanM <elhan.mahmutovic03@gmail.com>
2022-07-11 11:15:48 +0200

    added error handling via middleware in posts controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,36 +1,44 @@
 const Post = require("../models/Post");
 
-const getAllPosts = async (req, res) => {
-  const posts = await Post.find({});
-  res.status(200).json({ posts });
+const getAllPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ posts });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const createPost = async (req, res) => {
-  const post = await Post.create(req.body);
-  res.status(201).json({ post });
+const createPost = async (req, res, next) => {
+  try {
+    const post = await Post.create(req.body);
+    res.status(201).json({ post });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const getPost = async (req, res) => {
+const getPost = async (req, res, next) => {
   try {
     const { id } = req.params;
     const post = await Post.findOne({ _id: id });
     res.status(200).json({ post });
   } catch (error) {
-    res.status(404).json({ msg: `No post with id : ${id}`, status: 404 });
+    return next(new ErrorResponse(`No post with id : ${id}`, 404));
   }
 };
 
-const deletePost = async (req, res) => {
+const deletePost = async (req, res, next) => {
   try {
     const { id } = req.params;
     const post = await Post.findOneAndDelete({ _id: id });
     res.status(200).json({ post });
   } catch (error) {
-    res.status(404).json({ msg: `No post with id : ${id}`, status: 404 });
+    return next(new ErrorResponse(`No post with id : ${id}`, 404));
   }
 };
 
-const updatePost = async (req, res) => {
+const updatePost = async (req, res, next) => {
   try {
     const { id } = req.params;
     const post = await Post.findOneAndUpdate({ _id: id }, req.body, {
--- server/models/Post.js
+++ server/models/Post.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
 
 const postSchema = new mongoose.Schema(
   {
@@ -12,6 +13,11 @@ const postSchema = new mongoose.Schema(
     },
     photo: {
       type: String,
+      required: true,
+    },
+    user: {
+      type: ObjectId,
+      ref: "User",
       required: true,
     },
   },
